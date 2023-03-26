226c2d2edce36551651dffcb52aa4fe6bb5321e5
ewerton <ewerton.lost2@gmail.com>
2023-03-25 21:26:28 -0300

    update

--- portifolio.js
+++ portifolio.js
@@ -1,48 +1,82 @@
-/* Home Page*/
+/* Projects*/
 
-/*Aboute-me*/
+/*section*/
 
-/*Contacte-me*/ 
+document.querySelectorAll('.carousel').forEach(carousel =>{
+   const items = carousel.querySelectorAll("carousel-front,.carousel-back,.carousel-full")
+   const buttonsHtml = Array.from(items, () => {
+     return `<button class="carousel__button"></button>`;
+    });
+    console.log(buttonsHtml);
+    console.log(items)
 
-/*Projects*/
-document.querySelectorAll(".projects").forEach(projects=>{
-    const items= projects.querySelectorAll(".image-projects")
-    /* console.log(items) */
-    //vamos criar uma matrix de 3 items
-   /*  Cria um array com a mesma quantidade de itens da constante "items", cada um contendo um span com a classe "carousel-button". Essa constante será usada para criar os botões de navegação. */
-    const buttonHtml = Array.from(items,()=>{
-        return `<span class="carousel-button"></span>`
-    })
-    
-   /*  console.log(buttonHtml) */
-    
-   
-   //Insere no final do elemento "projects" um novo elemento com a classe "carousel-nav" e os botões de navegação criados anteriormente.
-    projects.insertAdjacentHTML("beforeend",`
-    <div class="carousel-nav">
-    ${buttonHtml.join("")}
+    carousel.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="carousel__nav">
+        ${buttonsHtml.join("")}
+      </div>
     `)
-    
-   /*  console.log(buttonHtml) */
-
-   const buttons = projects.querySelectorAll(".carousel-button");
-
-   buttons.forEach((button ,i)=>{
-    button.addEventListener("click",()=>{
-        //selecionando todos os items
-        items.forEach(item => item.classList.remove("carousel-selected"));
-        buttons.forEach(button=>button.classList.remove("carousel-button-selected"))
-
-        items[i].classList.add("carousel-selected");
-        button.classList.add("carousel-button-selected")
-    })
-
-        items[0].classList.add("carousel-selected");
-        button.classList.add("carousel-button-selected")
-   })
-
-   
-
-
 })
 
+
+//about me:
+//selection Loop Name
+/* Para realizar essa animação, a função usa um conjunto de variáveis e lógica condicional para manipular o conteúdo do elemento HTML que exibe o texto */
+const textDisplay = document.getElementById("initial-text");
+const phrases = ["FrontEnd","BackEnd","FullStack"];
+
+let i = 0;
+let j = 0;
+let currentPhrase = [];
+let isDeleting = false;
+let isEnd = false;
+
+function loop() {
+  isEnd = false;
+
+  /* O próximo passo é atualizar o conteúdo do elemento HTML que exibe o texto. Para isso, a função usa o método join do array currentPhrase para transformar as letras atuais em uma string REMOVER AS VIRGULAS e, em seguida, atribui essa string ao elemento HTML usando a propriedade innerHTML */
+
+  textDisplay.innerHTML = currentPhrase.join("");
+      //Aceessando Array
+     /*  O bloco condicional principal da função verifica se ainda há palavras a serem exibidas em phrases. Se não houver, a animação termina e a função não faz nada. */
+    if (i < phrases.length) {
+    /*  console.log(phrases[i]) */
+    if (!isDeleting && j <= phrases[i].length) {
+      /* console.log(phrases[i][j]) */
+      currentPhrase.push(phrases[i][j]);
+      j++;
+      console.log("add");
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (isDeleting && j <= phrases[i].length) {
+      currentPhrase.pop(phrases[i][j]);
+      j--;
+      console.log("remove");
+      textDisplay.innerHTML = currentPhrase.join("");
+    }
+
+    if (j == phrases[i].length) {
+      isEnd = true;
+      isDeleting = true;
+    }
+
+    if (isDeleting && j === 0) {
+      currentPhrase = [];
+      isDeleting = false;
+      i++;
+    }
+    if (i == phrases.length) {
+      i = 0;
+    }
+  }
+
+  const speedUp = Math.random() * (80 - 50) + 50;
+  const normalSpeed = Math.random() * (300 - 200) + 200;
+  const time = isEnd ? 2000 : isDeleting ? speedUp : normalSpeed;
+
+  setTimeout(loop, time);
+}
+
+loop();
